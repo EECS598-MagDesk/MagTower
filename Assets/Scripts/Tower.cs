cc8244501e812767f8622439bec71a999e525bcb
FrankHuangHKP <33185890+FrankHuangHKP@users.noreply.github.com>
2023-04-14 18:15:28 -0400

    update tower color

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -86,7 +86,7 @@ public class Tower : MonoBehaviour
 
     float getCurrentProjectileRange()
     {
-        float range = 10f + (transform.position.y) * 10;
+        float range = 10f + (transform.position.y) * 6;
         if (range > 60)
         {
             return 60;
@@ -99,7 +99,7 @@ public class Tower : MonoBehaviour
 
     float getCurrentAttackPeriod()
     {
-        float speed = 0.05f + (transform.position.y) * 0.05f;
+        float speed = 0.05f + (transform.position.y) * 0.1f;
         if (speed > 1)
         {
             return 1;
